bcd1196b6924e060923e41a02421a5be73347094
Dave Kang <hhkang94@gmail.com>
2021-08-19 12:09:26 +0900

    update

--- Basic/BAEKJOON/DFS와BFS.cpp
+++ Basic/BAEKJOON/DFS와BFS.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -24,12 +25,13 @@ int main(){
     cin>>M;
     cin>>V;
     
-    cout<<"\nN: "<<N<<"\tM:  "<<M<<"\tV: "<<V<<endl;
+//    cout<<"\nN: "<<N<<"\tM:  "<<M<<"\tV: "<<V<<endl;
     
     int u;
     int v;
     
-    vector<vector<int>> adj(M);
+    vector<vector<int>> adj(M+1);
+    vector<vector<int>> adj2(M+1);
     
     for (int i = 0;i < M;i++){
         cin>>u;
@@ -37,30 +39,37 @@ int main(){
         
         adj[u].push_back(v);
         adj[v].push_back(u);
+        adj2[u].push_back(v);
+        adj2[v].push_back(u);
     }
     
-    // sort number order for convenience
+    
+//    // sort number order for convenience
     for (int i = 1; i<=N; i++){
-        sort(adj[i].begin(),adj[i].end(),greater<int>());
+        sort(adj[i].begin(),adj[i].end(),less<int>());
+    }
+
+    for (int i = 1; i<=N; i++){
+        sort(adj2[i].begin(),adj2[i].end(),greater<int>());
     }
     
-    for (int i = 0; i < adj.size(); i++)
-        {
-            cout<< i << ": ";
-            
-            for (int j = 0; j < adj[i].size(); j++)
-            {
-                cout << adj[i][j] << " ";
-            }
-            cout << endl;
-        }
-    
-    cout<<"size: "<<adj.size()<<endl;
+//    for (int i = 0; i <= N; i++)
+//        {
+//            cout<< i << ": ";
+//
+//            for (int j = 0; j < adj2[i].size(); j++)
+//            {
+//                cout << adj2[i][j] << " ";
+//            }
+//            cout << endl;
+//        }
+
+//    cout<<"size: "<<adj.size()<<endl;
 
 // DFS
 
     // initialize stack with start point
-    vector<int> answer = {V};
+    vector<int> answer_dfs;
     vector<int> s = {V};
     
 //    // DFS with while loop and stack
@@ -68,18 +77,50 @@ int main(){
         int cv = s.back();
         s.pop_back();
         
+//        for (int i = 0; i<s.size();i++){
+//            cout<<s[i]<<" ";
+//        }
+//        cout<<endl;
+        
         if (!adj[cv].empty()){
-            s.push_back(adj[cv].back());
-            answer.push_back(adj[cv].back());
-            for (int i = 0; i < adj[cv].size(); i++)
-                adj[adj[cv][i]].pop_back();
+            answer_dfs.push_back(cv);
+            while(!adj[cv].empty()){
+                s.push_back(adj[cv].back());
+                adj[cv].pop_back();
+            }
         }
     }
     
-    for (int i = 0; i < answer.size(); i++)
-        cout<<answer[i]<< " ";
+    for (int i = 0; i < answer_dfs.size(); i++)
+        cout<<answer_dfs[i]<< " ";
     
     cout<<endl;
 
+    // DFS
+
+    // initialize stack with start point
+    vector<int> answer_bfs;
+    queue<int> q;
+    q.push(V);
+    
+//    // DFS with while loop and stack
+    while (!q.empty()){
+        int cv2 = q.front();
+        q.pop();
+        
+        if (!adj2[cv2].empty()){
+            answer_bfs.push_back(cv2);
+            while(!adj2[cv2].empty()){
+                q.push(adj2[cv2].back());
+                adj2[cv2].pop_back();
+            }
+        }
+    }
+    
+    for (int i = 0; i < answer_bfs.size(); i++)
+        cout<<answer_bfs[i]<< " ";
+    
+    cout<<endl;
+    
     return 0;
 }
